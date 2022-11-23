3ce91d3ad8e12d7965c5e917aa32a29d633df37c
ClementCornet <clement.cornet@edu.devinci.fr>
2022-11-23 17:55:24 +0100

    pre-deploy commit

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 with st.sidebar:
     #st.image('carott.png')
